f7711d23fbd50e310cd98aa58501a1cc6d3f9c6c
Hojin Lee <19949034+hjlee1371@users.noreply.github.com>
2024-10-09 15:37:33 +0000

    modify default config for small-scale proxy

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ from torch.distributed import init_process_group, destroy_process_group
 from llama import LlamaConfig, Llama
 
 # -----------------------------------------------------------------------------
-# default config values designed to train a gpt2 (124M) on OpenWebText
+# default config values following https://arxiv.org/abs/2309.14322
 # I/O
 out_dir = 'out'
 eval_interval = 2000
@@ -38,36 +38,36 @@ log_interval = 1
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'scratch' # 'scratch' or 'resume'
 # wandb logging
-wandb_log = False # disabled by default
-wandb_project = 'owt'
-wandb_run_name = 'gpt2' # 'run' + str(time.time())
+wandb_log = False
+wandb_project = 'small-scale-proxy'
+wandb_run_name = 'baseline' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
-dataset = 'shakespeare_char'
-gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
-block_size = 1024
+gradient_accumulation_steps = 16 # used to simulate larger batch sizes
+batch_size = 16 # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 512
 # model
-n_layer = 12
-n_head = 12
-n_embd = 768
+base_n_layer = 6
+base_n_head = 8
+base_n_embd = 512
+scale_factor = 1
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
-bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
-learning_rate = 6e-4 # max learning rate
-max_iters = 600000 # total number of training iterations
-weight_decay = 1e-1
+learning_rate = 1e-4 # max learning rate
+max_iters = 100000 # total number of training iterations
+weight_decay = 1e-4
+independent_weight_decay = True
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 z_loss = 1e-4
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 2000 # how many steps to warm up for
-lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
-min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+warmup_iters = 5000 # how many steps to warm up for
+lr_decay_iters = 100000 # should be ~= max_iters per Chinchilla
+min_lr = 1e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
@@ -78,6 +78,13 @@ compile = True # use PyTorch 2.0 to compile the model to be faster
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
+
+n_layer = base_n_layer * scale_factor
+n_head = base_n_head * scale_factor
+n_embd = base_n_embd * scale_factor
+if independent_weight_decay:
+    # some hack to use independent weight decay with pytorch optims
+    weight_decay = weight_decay / learning_rate
 # -----------------------------------------------------------------------------
 
 # various inits, derived attributes, I/O setup
