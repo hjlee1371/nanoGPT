5a41836608ab89e4acf86f899f6edc2576bf9274
Hojin Lee <19949034+hjlee1371@users.noreply.github.com>
2024-10-10 23:40:00 +0900

    fix multi-gpu logging

--- train.py
+++ train.py
@@ -252,10 +252,11 @@ def get_lr(it):
     return min_lr + coeff * (learning_rate - min_lr)
 
 # logging
-tb_logger = SummaryWriter(log_dir=out_dir)
-if wandb_log and master_process:
-    import wandb
-    wandb.init(project=wandb_project, name=wandb_run_name, config=config)
+if master_process:
+    tb_logger = SummaryWriter(log_dir=out_dir)
+    if wandb_log:
+        import wandb
+        wandb.init(project=wandb_project, name=wandb_run_name, config=config)
 
 # training loop
 X, Y = get_batch('train') # fetch the very first batch
